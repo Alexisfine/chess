3843ab4e855b2938ae4492d362ef4dff7b435931
Alexisfine <89954228+Alexisfine@users.noreply.github.com>
2023-11-26 21:33:01 -0500

    Edit Pawn

--- Pawn.cc
+++ Pawn.cc
@@ -1,7 +1,26 @@
 #include "Pawn.h"
 
 Pawn::Pawn(ChessBoard& board, Player& owner) :
-        ChessPiece{ChessType::PAWN, board, owner} {}
+        ChessPiece{ChessType::PAWN, board, owner} {
+    switch (owner.getPlayerNo()) {
+        case 0:
+            moveDx = 0;
+            moveDy = 1;
+            break;
+        case 1:
+            moveDx = 0;
+            moveDy = -1;
+            break;
+        case 2:
+            moveDx = 1;
+            moveDy = 0;
+            break;
+        case 3:
+            moveDx = -1;
+            moveDy = 0;
+            break;
+    }
+}
 
 bool Pawn::isMovePossiblyValid(const Move& move) {
     std::vector<ValidMove> possibleValidMoves = getAvailableMoves(move.getStart());
@@ -13,7 +32,7 @@ bool Pawn::isMovePossiblyValid(const Move& move) {
 
 std::vector<ValidMove> Pawn::getAvailableMoves(const Position& curPosition) {
     std::vector<ValidMove> moves;
-    Position newPosition {curPosition.getRow() + 1, curPosition.getCol() + 1};
+    Position newPosition {curPosition.getRow() + moveDy, curPosition.getCol() + moveDx};
     if (!board.isValidPos(newPosition)) return moves; // check if newPosition is within the grid
     if (!board.isPositionEmpty(newPosition)) return moves;
 
--- Pawn.h
+++ Pawn.h
@@ -3,6 +3,8 @@
 #include "ChessPiece.h"
 
 class Pawn : public ChessPiece {
+    int moveDx, moveDy;
+public:
     Pawn(ChessBoard& board, Player& player);
     bool isMovePossiblyValid(const Move& move) override;
     std::vector<ValidMove> getAvailableMoves(const Position& curPosition) override;
